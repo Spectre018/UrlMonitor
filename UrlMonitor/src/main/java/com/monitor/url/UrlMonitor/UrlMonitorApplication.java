a27b103435273f372f19f7fcc454e096b43dcc2f
admin <ashishkarn>
2022-07-30 10:45:53 +0530

    first commit

---
+++ UrlMonitor/src/main/java/com/monitor/url/UrlMonitor/UrlMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.monitor.url.UrlMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlMonitorApplication.class, args);
+	}
+
+}
---
+++ UrlMonitor/src/test/java/com/monitor/url/UrlMonitor/UrlMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitor.url.UrlMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
