2dea86a40bb68d00c0de46ddb896a008f54ce7df
Spectre018 <46166527+Spectre018@users.noreply.github.com>
2022-08-02 11:57:58 +0530

    Add files via upload
    Updated pagination property in service

--- UrlMonitor/src/main/java/com/monitor/url/UrlMonitor/Service/UrlService.java
+++ UrlMonitor/src/main/java/com/monitor/url/UrlMonitor/Service/UrlService.java
@@ -1,70 +1,95 @@
 package com.monitor.url.UrlMonitor.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Service;
 
+import com.monitor.url.UrlMonitor.helper.UrlCount;
+
 @Service
 public class UrlService {
 
-	static final Map<String, String> shortToLongMap = new ConcurrentHashMap<>();
-	static final Map<String, Integer> shortToCount = new ConcurrentHashMap<>();
-    static final Map<String, String> longToShortMap = new ConcurrentHashMap<>();
-    public static final Map<String, Integer> longAndCount = new ConcurrentHashMap<>();
+    public static final Map<String, String> longToShortMap = new ConcurrentHashMap<>();
+    static final Map<String, Integer> longAndCount = new ConcurrentHashMap<>();
     static int counter = 0;
 
     // Encodes a URL to a shortened URL.
-    public String encode(String longUrl)
+    public String storeUrl(String longUrl)
 	{
         if(longToShortMap.containsKey(longUrl))
+        {
             return longToShortMap.get(longUrl);
-        
-//        String shortUrl = convertDecimalToBase62(counter++);
+        }
         String shortUrl = convertDecimalToBase62(counter++) + "a";
-        
-        shortToLongMap.put(shortUrl, longUrl);
         longToShortMap.put(longUrl, shortUrl);
-        shortToCount.put(shortUrl, 0);
         longAndCount.put(longUrl, 0);
-        System.out.println("shortUrl = "+shortUrl);
         return shortUrl;
     }
 
+    
+    // Returns the visit count of the url
     public int count(String longUrl) {
-    	String shortUrl="";
-    	if(longToShortMap.size()>0) {
-    		shortUrl = longToShortMap.get(longUrl);
-    	}
-    	int visit = 0;
-    	if(shortUrl!=null) {
-    		if(shortToCount.containsKey(shortUrl)) {
-    		visit = shortToCount.get(shortUrl);
-    		}
+    	int visit = -1;
+    	if(longAndCount.containsKey(longUrl)) {
+    		visit = longAndCount.get(longUrl);
+    		return visit;
     	}
     	return visit;
-    	
     }
     
     // Check for a short url 
     public String getLongToShortUrl(String longUrl) {
-    	if(longToShortMap.size()>0) {
-    		String shortUrl = longToShortMap.get(longUrl);
-        	shortToCount.put(shortUrl, shortToCount.get(shortUrl)+1);
-        	longAndCount.put(longUrl, longAndCount.get(longUrl)+1);
-        	return shortUrl;
+    	String shortKey = null;
+    	if(longToShortMap.containsKey(longUrl)) {
+    		shortKey = longToShortMap.get(longUrl);
+    		longAndCount.put(longUrl, longAndCount.get(longUrl)+1);
     	}
-    	return null;
+    	return shortKey;
     }
     
     
-    // List all urls with visit count
-    public void allUrlsAndCounts() {
-    	for(Map.Entry<String, String> list : longToShortMap.entrySet()) {
-    		String longUrl = list.getKey();
-    		int count = shortToCount.get(list.getValue());
-    		System.out.println("longUrl = "+longUrl+"  "+"count = "+count);
+    // Returns List all urls with visit count
+    public static Map<String, Integer> getListOfUrls(int page, int size)
+    {
+    	// collection to collect urls and its current visit count to implement pagination
+    	List<UrlCount> longCountList = Collections.synchronizedList(new ArrayList<UrlCount>());
+    	for(Map.Entry<String, Integer> value : longAndCount.entrySet()) {
+    		longCountList.add(new UrlCount(value.getKey(), value.getValue()));
     	}
+
+    	int longCountList_size = longCountList.size();
+    	int countUptoLastPage = size*(page-1);
+    	boolean lastPage = longCountList_size-countUptoLastPage<=size;
+    	boolean outOfBound = countUptoLastPage > longCountList_size;
+    	int start = size*(page-1);
+    	int end = longCountList_size%size;
+    	Map<String, Integer> listMap = new ConcurrentHashMap<>();
+    	if(outOfBound) {  // when url index is out of bound
+    		System.out.println("outofbound");
+    		return listMap;
+    	}
+    	if(lastPage) {    // When the url is in the last page
+    		start = size*(page-1);
+    		end = longCountList_size;
+    		for(int i=start;i<end;i++) {
+    			listMap.put(longCountList.get(i).url, longCountList.get(i).count);
+    		}
+    		return listMap;
+    	}
+    	start = size*(page-1);
+    	end = size*(page-1) + size;
+    	
+    	for(int i=start; i<end;i++) {
+    		listMap.put(longCountList.get(i).url, longCountList.get(i).count);
+    	}
+    	return listMap;
+    	
+    	
+
     }
     
     
